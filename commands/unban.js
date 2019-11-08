48741e97f6fad8478d0c2872bddffec784f1ccf4
kickz1997 <55470383+kickz1997@users.noreply.github.com>
2019-11-07 21:41:59 -0600

    Update unban.js

--- commands/unban.js
+++ commands/unban.js
@@ -1,1 +1,15 @@
+const Discord = require('discord.js')
 
+module.exports.run = async (client, message, args, guild) => {
+  
+  let userid = message.mentions.users.first().id
+  if(!userid) return message.reply('error?')
+  
+  // Unban a user by ID (or with a user/guild member object)
+guild.unban(userid)
+  .then(user => message.channel.send(`Unbanned ${user.username} from ${guild}`))
+  .catch(console.error);
+}
+module.exports.help = {
+  "name": "unban"
+}
