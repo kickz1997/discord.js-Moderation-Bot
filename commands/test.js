7ebaa809ecff4e64b687e87c5dd2699d9570b61d
kickz1997 <55470383+kickz1997@users.noreply.github.com>
2019-11-07 20:19:27 -0600

    Update test.js

--- commands/test.js
+++ commands/test.js
@@ -1,1 +1,17 @@
+const Discord = require('discord.js')
 
+module.exports.run = async (client, message, msg, args) => {
+
+  let embed = new Discord.RichEmbed()
+  .setTitle("Test")
+  .setColor('RANDOM")
+  .setDescription("This command is to see if the bot is online! If it is not online then this command would not work!")
+   .addField("Testing","Hello, I am online and working! There is no need to test me!")
+    .setFooter("Thank You for testing the bot!")
+     message.channel.send(embed)
+  message.delete()
+}
+
+module.exports.help = {
+  "name": "test"
+}
