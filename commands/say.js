d1edc594b4d1d6b78c3fc604253b3580554416ca
kickz1997 <55470383+kickz1997@users.noreply.github.com>
2019-11-07 19:23:43 -0600

    Update say.js

--- commands/say.js
+++ commands/say.js
@@ -1,1 +1,19 @@
+const Discord = require("discord.js");
 
+module.exports.run = async (client, message, args) => {
+  
+      message.delete().catch(O_o=>{});
+  
+  if(message.author.id === "379784651792711700") {
+   const sayMessage = args.join(" ");
+    // Then we delete the command message (sneaky, right?). The catch just ignores the error with a cute smiley thing.
+    message.delete().catch(O_o=>{}); 
+    // And we get the bot to say the thing: 
+    message.channel.send(sayMessage);
+  }
+}
+
+
+module.exports.help = {
+  name: "say"
+}
