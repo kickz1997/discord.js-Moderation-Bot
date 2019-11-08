8ad8a0d212d866e763c607b501271b85b9573ea6
kickz1997 <55470383+kickz1997@users.noreply.github.com>
2019-11-07 19:14:19 -0600

    Update eval.js

--- commands/eval.js
+++ commands/eval.js
@@ -1,1 +1,25 @@
+const Discord = require("discord.js");
 
+exports.run = async (client, message, args, color, prefix) => {
+    if (message.author.id !== '379784651792711700') {
+    try {
+        let codein = args.join(" ");
+        let code = eval(codein);
+
+        if (typeof code !== 'string')
+            code = require('util').inspect(code, { depth: 0 });
+        let embed = new Discord.RichEmbed()
+        .setAuthor('Evaluate')
+        .setColor('RANDOM')
+        .addField(':inbox_tray: Input', `\`\`\`js\n${codein}\`\`\``)
+        .addField(':outbox_tray: Output', `\`\`\`js\n${code}\n\`\`\``)
+        message.channel.send(embed)
+    } catch(e) {
+        message.channel.send(`\`\`\`js\n${e}\n\`\`\``);
+    }
+    }
+}
+
+module.exports.help = {
+    "name": "eval"
+    } 
