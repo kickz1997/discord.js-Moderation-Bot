62eb5029115c10b675340eacb9807f0606b9a6bf
kickz1997 <55470383+kickz1997@users.noreply.github.com>
2019-11-07 21:44:50 -0600

    Update uptime.js

--- commands/uptime.js
+++ commands/uptime.js
@@ -1,1 +1,22 @@
+const Discord = require("discord.js");
 
+module.exports.run = async (client, message, args) => {
+  if (!message.member.permissions.has("ADMINISTRATOR")) return message.channel.send("I am sorry but you cannot access this command!")
+  
+  let totalSeconds = (client.uptime / 1000);
+let days = Math.floor(totalSeconds / 86400);
+let hours = Math.floor(totalSeconds / 3600);
+totalSeconds %= 3600;
+let minutes = Math.floor(totalSeconds / 60);
+let seconds = totalSeconds % 60;
+  
+  message.channel.send(`${days} days, ${hours} hours, ${minutes} minutes and ${seconds} seconds`)
+  
+  
+  message.delete().catch(O_o => {});
+
+    }
+
+module.exports.help = {
+  "name": "uptime"
+}
