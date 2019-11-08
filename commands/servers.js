6588c596442d12a7d252fe3e77a8258dc00348e5
kickz1997 <55470383+kickz1997@users.noreply.github.com>
2019-11-07 22:11:50 -0600

    Update servers.js

--- commands/servers.js
+++ commands/servers.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 
 module.exports.run = async (client, message, args) => {
   
-if(message.author.id != "379784651792711700") return message.channel.send("I am sorry but you do not have access to this command!")
+if(message.author.id != "Your ID here!") return message.channel.send("I am sorry but you do not have access to this command!")
   message.channel.send(`Serving ${client.guilds.size} servers`)
   message.channel.send(client.guilds.map(g=>g.name).join('\n'))
   }
