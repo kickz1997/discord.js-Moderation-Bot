c126730fd14638e8aae27cccd6eeff3dec951c28
kickz1997 <55470383+kickz1997@users.noreply.github.com>
2019-11-07 21:42:53 -0600

    Update unmute.js

--- commands/unmute.js
+++ commands/unmute.js
@@ -1,1 +1,25 @@
+const Discord = require("discord.js");
+const ms = require("ms");
 
+module.exports.run = async (client, message, args) => {
+  
+if(message.member.roles.some(r=>["Rel Staff"].includes(r.name)) ) {
+  
+let role = message.guild.roles.find(r => r.name === "muted");
+
+// Let's pretend you mentioned the user you want to add a role to (!addrole @user Role Name):
+let member = message.mentions.members.first();
+  
+  if(!member) return message.reply("Could not find that user")
+// Remove a role!
+member.removeRole(role).catch(console.error);
+  message.reply(`I have unmuted ${member} successfully!`)
+  } else {
+    message.reply("You do not have permissions to access this command!")
+  }
+
+};
+
+module.exports.help = {
+  name: "unmute"
+}
