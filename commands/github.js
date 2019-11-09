3234b87b9dac29e47938a09eafe4cd078415ddb8
kickz1997 <55470383+kickz1997@users.noreply.github.com>
2019-11-08 21:34:38 -0600

    Update github.js

--- commands/github.js
+++ commands/github.js
@@ -1,1 +1,24 @@
+const Discord = require('discord.js')
 
+module.exports.run = async (client, message, msg, args) => {
+  
+    let member = message.author
+  
+  let something = new Discord.RichEmbed()
+  .setTitle("Invite for the bot!")
+  .setColor("RANDOM")
+  .setDescription("This command sends you a dm with a link to My Creator's First Repo!")
+  .addField("Here is my Creator's first GitHub!", "Your GitHub Link here!")
+  .setFooter("Whatever you want here!")
+  
+  message.delete();
+  
+  
+  message.reply(":mailbox_with_mail: I have dm you the link of my Creator's First Repo!").then(msg => {msg.delete(10000)})
+  member.send(something)
+}
+
+
+module.exports.help = {
+"name": "github"
+}
