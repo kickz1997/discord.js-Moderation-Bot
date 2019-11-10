f66d1129f00684aa8b5c8468a70e7aa6c8b697ca
kickz1997 <55470383+kickz1997@users.noreply.github.com>
2019-11-09 22:29:39 -0600

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const { prefix, config, token } = require('./config.json');
-const Enmap = require('enmap');
+const { prefix, config, token } = require("./config.json");
+const Enmap = require('enmap')
 const fs = require('fs');
 const express = require('express');
 const app = express();
@@ -11,8 +11,8 @@ app.get('/', function(req, res) {
 });
 
 const listener = app.listen(process.env.PORT, function() {
-  console.log("Your app is listening on port " + listener.address().port);
-})
+  console.log('Your app is listening on port ' + listener.address().port);
+});
 
 client.commands = new Enmap();
 
@@ -20,21 +20,24 @@ fs.readdir("./commands/", (err, files) => {
   if (err) return console.error(err);
   files.forEach(file => {
     if (!file.endsWith(".js")) return;
+    // Load the command file itself
     let props = require(`./commands/${file}`);
+    // Get just the command name from the file name
     let commandName = file.split(".")[0];
     console.log(`Attempting to load command ${commandName}`);
+    // Here we simply store the whole thing in the command Enmap. We're not running it right now.
     client.commands.set(commandName, props);
   });
 });
 
-client.on("ready", () => {
-console.log(`Logged in as ${client.user.tag}!`);
-  client.user.setActivity(client.guilds.size + " Servers!" , {type:"LISTENING", url:"https://discordapp.com/"})//you can change this!
-  });
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+  client.user.setActivity(whatever status you want here! + " Users!" , {type: 'WATCHING', url: "https://twitch.tv/kickz19"}) // good. check the bot works //Why are you messing with it lol
+});
 
 client.on('message', async (msg, message, channel) => {
   
-  if (msg.content === '/ping') { //you Must change the prefix behind ping to your prefix!
+  if (msg.content === '/ping') { //remember to change the prefix
   msg.reply('Ping?').then(m => m.edit(`${msg.author}, Pong! Latency is ${m.createdTimestamp - msg.createdTimestamp}ms. API Latency is ${Math.round(client.ping)} ms`))
   }
   
@@ -48,7 +51,7 @@ client.on('message', async (msg, message, channel) => {
 });
 
 client.on("message", (message, args) => {
-    if (message.content.startsWith("/ban")) { //you Must change the prefix behind ban to your prefix!
+    if (message.content.startsWith("/ban")) {//remember to change the prefix
 
         // Easy way to get member object though mentions.
         var member = message.mentions.members.first();
@@ -58,10 +61,10 @@ client.on("message", (message, args) => {
             message.channel.send(":wave: " + member.displayName + " has been successfully banned https://gfycat.com/playfulfittingcaribou :point_right: ");
         }).catch((error) => {
              // Failmessage
-           message.channel.send("Error:\n```fix\n" + error + "\n```");
+            message.channel.send("Error:\n```fix\n" + error + "\n```");
         });
     }
- if (message.content.startsWith("/kick")) { //you Must change the prefix behind kick to your prefix!
+ if (message.content.startsWith("/kick")) { //remember to change the prefix!
         // Easy way to get member object though mentions.
         var member = message.mentions.members.first();
         // ban
@@ -70,10 +73,10 @@ client.on("message", (message, args) => {
             message.channel.send(":wave: " + member.displayName + " has been successfully kicked :point_right: ");
         }).catch((error) => {
              // Failmessage
-          message.channel.send("Error:\n```fix\n" + error + "\n```");
+            message.channel.send("Error:\n```fix\n" + error + "\n```");
         });
     }
-}); 
+}); //that is the right code why it not working
 
 
 
